f6ceaff762c0c8f87f622c913a9eace4bf0f08ca
Daniel Cui <solunian@gmail.com>
2024-09-07 10:06:03 -0700

    ops: add vercel analytics snippet

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
   import "../app.css";
+
+  // vercel analytics snippet
+  import { dev } from "$app/environment";
+  import { inject } from "@vercel/analytics";
+
+  inject({ mode: dev ? "development" : "production" });
 </script>
 
 <slot />
